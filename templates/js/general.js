52ec3c3a05da492b538a9b2ee3eaa4a0d9516ccb
gaspard <gaspard@gmail.com>
2013-02-09 15:15:12 +0100

    experimentations

--- templates/js/general.js
+++ templates/js/general.js
@@ -8,7 +8,7 @@
 var proportio={
 	image: {
 		name: null,
-		path: 'http://proportio.site/iconography/',
+		path: '/iconography/',
 //		path: 'http://freelancis.net/~franck/proportio/iconography/',
 		sizes:[{w: 640,h: 480},{w: 800,h: 600},{w:1024,h: 768},{w:1280,h: 960},{w:1600,h:1200},{w:2048,h:1536}]
 	},
@@ -22,6 +22,11 @@ var proportio={
 	place: function(){
 		this.display.w = $(document).width();
 		this.display.h = $(document).height();
+		
+		/*
+			WARNING : document and window do not have the same dimension
+		*/
+		
 		// are we in a portrait or in landscape display (viewport?)
 		this.display.landscape = (this.display.w*3>this.display.h*4);
 
@@ -30,6 +35,9 @@ var proportio={
 		
 	},
 	load: function(){
+		
+		console.log(proportio.choose());
+		
 		
 		var img = new Image();
 		$(img).load(function () {
@@ -47,9 +55,13 @@ var proportio={
 		 if we are in landscape viewport mode, we gonna choose the image depending on the width
 		 if we are in portrait, let's choose depending on the height
 		*/
+		console.log(this);
+		
 		var i=0;
 		while((!this.display.landscape && (this.display.h > this.image.sizes[i].h))
 			||(this.display.landscape && (this.display.w > this.image.sizes[i].w))){
+			console.log(i);
+			
 			i++;
 		}
 		console.log({display:this.display,selectedImage:this.image.sizes[i]});
@@ -66,7 +78,9 @@ var proportio={
 
 
 $(document).ready(function(){
-	proportio.image.name= $('body').attr('id');
+	
+	// general display
+	proportio.image.name= $('body').attr('id'); // this won't be working anymoye
 	$('#b').css({backgroundColor:proportio.display.bgcolor});
 	proportio.place();
 	proportio.load();
@@ -76,6 +90,23 @@ $(document).ready(function(){
 		proportio.place();
 		proportio.load();
 	});
+	
+	// navigation
+	$('#navigation a').click(function(){
+		$('#navigation a').removeClass('active');
+		$(this).addClass('active');
+		$(window).scrollTo( $($(this).attr('href')), 800 );
+		return false;
+		/*
+			TODO use the hash after the scroll
+			it might be embed into the plugin
+		*/
+	});
+	if(window.location.hash){
+		$('#navigation a').removeClass('active');
+		$('a[href='+window.location.hash+']').addClass('active');
+	}
+	
 
 });
 

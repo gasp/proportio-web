1df238b68867a81196471f610ac202d4d233550c
gaspard <gaspard@gmail.com>
2013-03-06 12:07:19 +0100

    dates: don't need debug anymore

--- mes_fonctions.php
+++ mes_fonctions.php
@@ -7,8 +7,6 @@ function affdate_simple($d){
 	$d=strtotime($d);
 	$today = array('Y'=>date('Y'),'m'=>date('m'),'d'=>date('d'));
 	$date  = array('Y'=>date('Y',$d),'m'=>date('m',$d),'d'=>date('d',$d));
-	var_dump($date);
-	var_dump($today);
 	if($date['Y'] == $today['Y']) return date('M',$d).', '.date('dS',$d);
 	else return date('M',$d).' '.$date['Y'];
 }

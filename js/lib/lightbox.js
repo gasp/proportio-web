8e5ff17793e2de97f8f0dc33003b091ac5078e7f
gaspard <gaspard@gmail.com>
2013-03-09 12:36:43 +0100

    lightbox: installing

--- js/lib/lightbox.js
+++ js/lib/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileLoadingImage = '/squelettes/img/loading.gif';
+      this.fileCloseImage = '/squelettes/img/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";

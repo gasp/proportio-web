00700c0ccac89930e7f54654f6591dabda6d5a78
gaspard <gaspard@gmail.com>
2013-03-17 11:56:14 +0100

    urlrewriting : tweaking urls.

--- mes_fonctions.php
+++ mes_fonctions.php
@@ -1,5 +1,15 @@
 <?php
 
+define('_SPIP_SCRIPT','');
+define('_url_minuscules',1);
+define('_url_arbo_minuscules',1);
+define('_MARQUEUR_URL','');
+define('_url_propres_sep_id','-');
+define('_URLS_PROPRES_MIN', 0);
+define('_URLS_PROPRES_MAX', 80);
+define('_URLS_ARBO_MIN', 0);
+define('_URLS_ARBO_MAX', 80);
+
 $GLOBALS['quota_cache'] = 500;
 define('_DUREE_CACHE_DEFAUT', 24*3600*30);
 

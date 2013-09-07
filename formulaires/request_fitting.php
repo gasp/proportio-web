ac5f5cb86c3d2e33d173720ded6520d2292afd04
gaspard <gaspard@gmail.com>
2013-09-07 14:55:10 -0300

    form: request fitting preview

--- formulaires/request_fitting.php
+++ formulaires/request_fitting.php
@@ -1,16 +1,5 @@
 <?php
 
-/***************************************************************************\
- *  SPIP, Systeme de publication pour l'internet                           *
- *                                                                         *
- *  Copyright (c) 2001-2012                                                *
- *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
- *                                                                         *
- *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
- *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
-\***************************************************************************/
-
-
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function formulaires_request_fitting_charger_dist(){
@@ -25,9 +14,7 @@ function formulaires_request_fitting_charger_dist(){
 		'email_message_auteur'=>$GLOBALS['visiteur_session']['email']
 	);
 	
-	// id du formulaire (pour en avoir plusieurs sur une meme page)
-	$valeurs['id'] = "rfitting";
-	
+	$valeurs['rien'] = '';
 	return $valeurs;
 }
 
